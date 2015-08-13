8fb6d1bfac95e43b64642a1433008858ec027ddb
Kevin <quantus@live.cn>
2015-08-13 17:12:08 +0800

    delete

--- BigSim上传资料/BigSim-v0.1/src/org/bigraph/biggr/simulator/Components.scala
+++
@@ -1,9 +0,0 @@
-package org.bigraph.biggr.simulator
-
-import org.bigraph.biggr.bigraph._
-
-class Components {
-  var context:Bigraph = null
-  var matched:Bigraph = null
-  var parameter:Bigraph = null
-}
